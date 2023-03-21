774c8366a1efbe22d41a8eb1c0a4e0b171414a3b
WarunaNavarathne <navarathnewaruna7@gmail.com>
2023-03-22 00:54:37 +0530

    Added new firebase configuration

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyC3sYC-BI_p5iOgWQOHPDpjc0QB_eDIXkw",
-    authDomain: "casanovaapp-68898.firebaseapp.com",
-    projectId: "casanovaapp-68898",
-    storageBucket: "casanovaapp-68898.appspot.com",
-    messagingSenderId: "32605442797",
-    appId: "1:32605442797:web:d3428779a83acd8c6c72e4"
+    apiKey: "AIzaSyCkl6mE9txUeCf7Z0K4hTYDxpwqnhL3BL4",
+    authDomain: "casanovaapp-dfc2e.firebaseapp.com",
+    projectId: "casanovaapp-dfc2e",
+    storageBucket: "casanovaapp-dfc2e.appspot.com",
+    messagingSenderId: "376903640774",
+    appId: "1:376903640774:web:a8eea7ff1c49684fb199c7"
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
