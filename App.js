abf1046742597bff870713da1d6e067faee568c4
WarunaNavarathne <navarathnewaruna7@gmail.com>
2023-03-22 14:40:08 +0530

    Added the view function and navigation button

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import InsertRawMaterial from './app/screens/InsertRawMaterial';
+import ViewRawMaterials from './app/screens/ViewRawMaterials';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,7 +9,8 @@ export default function App() {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
-				<Stack.Screen name="Add raw materials" component={InsertRawMaterial} options={{headerShown: false}}/>
+				<Stack.Screen name="Viewrawmaterials" component={ViewRawMaterials} options={{headerShown: false}}/>
+				<Stack.Screen name="Addrawmaterials" component={InsertRawMaterial} options={{headerShown: false}}/>
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
